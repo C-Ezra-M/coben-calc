e86a9f97ee6af73cececae6b369046e1de3ff0bc
C-Ezra-M <wmarzec06.wm@gmail.com>
2024-07-08 10:11:53 +0200

    change terser plugin dep

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from '@rollup/plugin-terser';
 import css from 'rollup-plugin-css-only';
 import json from '@rollup/plugin-json';
 
