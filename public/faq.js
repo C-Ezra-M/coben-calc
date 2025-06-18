e293dff421ef31519049a277244a7c0e1a08087e
C-Ezra-M <wmarzec06.wm@gmail.com>
2025-06-18 14:14:12 +0200

    add FAQ

---
+++ public/faq.js
@@ -0,0 +1,8 @@
+tocbot.init({
+    tocSelector: '#toc',
+    contentSelector: 'main',
+    headingSelector: 'h2',
+    hasInnerContainers: false,
+    scrollSmooth: false,
+    scrollContainer: false,
+})
